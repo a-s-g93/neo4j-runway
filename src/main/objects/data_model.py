510f695274b0cdb5ba12af4f9a722eb836dc6e0c
alex <a.gilmore1993@gmail.com>
2024-02-24 17:33:26 -0600

    relationship validation and testing

--- src/main/objects/data_model.py
+++ src/main/objects/data_model.py
@@ -29,6 +29,22 @@ class DataModel(BaseModel):
             "nodes": [n.__dict__ for n in self.nodes],
             "relationships": [r.__dict__ for r in self.relationships],
         }
+    
+    @property
+    def node_labels(self) -> List[str]:
+        """
+        Return a list of the node labels.
+        """
+
+        return [n.label for n in self.nodes]
+    
+    @property
+    def relationship_types(self) -> List[str]:
+        """
+        Return a list of the relationship types.
+        """
+
+        return [r.type for r in self.relationships]
 
     def validate_model(self, csv_columns: List[str]) -> None:
         """
@@ -43,26 +59,39 @@ class DataModel(BaseModel):
         for rel in self.relationships:
             errors+=rel.validate_properties(csv_columns=csv_columns)
             errors+=rel.validate_unique_constraints(csv_columns=csv_columns)
-        # if len(errors) > 0:
-        #     print(errors)
-            # raise ValueError(str(errors))
+        
+        errors+=self.validate_relationship_sources_and_targets()
+        
         if len(errors) > 0:
             message = f"""
                     Fix the errors in following data model.
                     Data Model:
                     {self.dict}
                     Errors:
-                    {errors}
+                    {str(errors)}
                     """
             return {
                 "valid": False,
-                "message": str(errors)
+                "message": message
             }
         return {
             "valid": True,
             "message": ""
         }
-        
+    
+    def validate_relationship_sources_and_targets(self) -> List[Union[str, None]]:
+        """
+        Validate the source and target of a relationship exist in the model nodes.
+        """
+
+        errors = []
+        for rel in self.relationships:
+            if rel.source not in self.node_labels:
+                errors.append(f"relationship {rel.type} source {rel.source} does not exist in generated Node labels.")
+            if rel.target not in self.node_labels:
+                errors.append(f"relationship {rel.type} target {rel.target} does not exist in generated Node labels.")
+        return errors
+                
     def map_columns_to_values(self, column_mapping: Dict[str, str]) -> None:
         """
         Apply a column mapping to the node labels, relationship types and all properties.
