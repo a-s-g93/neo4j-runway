b1d1c479cdd70d5fe508cb6f5697e17bec2a2bd8
alex <a.gilmore1993@gmail.com>
2024-07-15 16:00:54 -0500

    black formatting

--- tests/test_models/test_solutions_workbench/test_solutions_workbench_relationship.py
+++ tests/test_models/test_solutions_workbench/test_solutions_workbench_relationship.py
@@ -98,10 +98,15 @@ class TestSolutionsWorkbenchRelationship(unittest.TestCase):
     def test_init_to_core_relationship(self) -> None:
         sw_rel = SolutionsWorkbenchRelationship(**self.json_input)
         node_id_to_label_map = {"Node0": "Person", "Node2": "Pet"}
-        core_rel = Relationship.from_solutions_workbench(solutions_workbench_relationship=sw_rel, node_id_to_label_map=node_id_to_label_map)
+        core_rel = Relationship.from_solutions_workbench(
+            solutions_workbench_relationship=sw_rel,
+            node_id_to_label_map=node_id_to_label_map,
+        )
 
         self.assertEqual(core_rel.type, sw_rel.type)
         self.assertEqual(len(core_rel.properties), len(sw_rel.properties.keys()))
         self.assertEqual(core_rel.csv_name, sw_rel.description)
-        self.assertEqual(core_rel.source, node_id_to_label_map[sw_rel.startNodeLabelKey])
+        self.assertEqual(
+            core_rel.source, node_id_to_label_map[sw_rel.startNodeLabelKey]
+        )
         self.assertEqual(core_rel.target, node_id_to_label_map[sw_rel.endNodeLabelKey])
